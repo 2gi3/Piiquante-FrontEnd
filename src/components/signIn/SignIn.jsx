767f134d510933e864051a3c7497609b437d9557
Giuseppe Ippolito <gippolito@hotmail.co.uk>
2022-05-25 01:37:47 +0100

    log in page ready with all functions

--- src/components/signIn/SignIn.jsx
+++ src/components/signIn/SignIn.jsx
@@ -1,30 +1,32 @@
 import "./signIn.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRightToBracket, faKey, faAt } from '@fortawesome/free-solid-svg-icons'
+import { faArrowRightToBracket, faKey, faAt, faUsers } from '@fortawesome/free-solid-svg-icons'
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 
 function SignIn() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [logInMessage, setLogInMessage] =useState('')
+    const [afterLogIn, setAfterLogIn] =useState('')
     const userData = {
         email,
         password
     }
 
-    const logIn = (e) => {
+    const logIn = (e,) => {
         e.preventDefault();
-        let data = {
-            email: email,
-            password: password,
-        }
-        console.log(data)
+        // let data = {
+        //     email: email,
+        //     password: password,
+        // }
+        console.log(userData)
         //  if(!re.test(email)){
         // 	  setEmailInvalid(` Please provide a valid email `)			  
         // 	}else if(!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password)){
         // 	      setPasswordWarning('Please provide a password that contains minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character')
         // 		}else{		
-        axios.post("https://secure-harbor-62492.herokuapp.com/api/auth/login", data)
+        axios.post("https://secure-harbor-62492.herokuapp.com/api/auth/login", userData)
             .then(
                 (res) => {
                     sessionStorage.setItem("token", res.data.token)
@@ -40,9 +42,28 @@ function SignIn() {
             });
         // }
     }
+
+    const signUp =(e) =>{
+        e.preventDefault()
+
+        axios.post("https://secure-harbor-62492.herokuapp.com/api/auth/signup", userData)
+        .then(
+            () => {
+               
+                console.log("user created");
+                setAfterLogIn('vanish')
+                setLogInMessage('Log in to read some hot reviews!')
+
+                // window.location = "/";
+            })
+        .catch((err) => {
+            console.log(err);
+        });
+    // }
+}
     return (
         <div className="container">
-            <div className="innerContainer">
+            <div className="inContainer">
                 <form onSubmit={event => logIn(event)}>
                     <div className="inputBox">
                         <input type="email" name="userEmail" placeholder="UserEmail"
@@ -54,7 +75,6 @@ function SignIn() {
                         </div>
                         {/* <h1>{emailInvalid}</h1> */}
                     </div>
-
                     <div className="inputBox">
                         <input type="password" name="password" placeholder="Password"
                             onChange={event => setPassword(event.target.value)}
@@ -65,6 +85,7 @@ function SignIn() {
                         </div>
                         {/* <h1>{passwordWarning}</h1> */}
                     </div>
+                    <div className="logInMessage"> <p>{logInMessage}</p></div>
                     <div className="logInSignUpButtons">
                         <button className="signInButton sauceButton " type="submit" value="submit">
                             <span>Login <FontAwesomeIcon icon={faArrowRightToBracket} /></span>
@@ -72,9 +93,9 @@ function SignIn() {
                     </div>
                 </form>
                 <div className="logInSignUpButtons">
-                <button className="signUpButton sauceButton"
-                    onClick={() => console.log(userData)}>
-                    <span>Sign up</span>
+                <button className="signUpButton sauceButton" id={afterLogIn}
+                    onClick={e => signUp(e)}>
+                    <span>Sign up <FontAwesomeIcon icon={faUsers} /></span>
                 </button>
             </div>
             </div>
