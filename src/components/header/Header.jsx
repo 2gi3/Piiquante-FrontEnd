f396bad5c697048a0ef1153d49a90892ef50813e
Giuseppe Ippolito <gippolito@hotmail.co.uk>
2022-11-08 08:13:46 +0000

    homepage animation improved

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -25,14 +25,23 @@ function NavBar() {
     }
 
     const arrayOfBlocks = { 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], }
-    // let blocks1 = []
-    // let blocks2 = []
-    // let blocks3 = []
-    // let blocks4 = []
-    // let blocks5 = []
-    // let blocks6 = []
-    // let blocks7 = []
-    // let blocks8 = []
+    let windowWidth={
+        size: 0
+    }
+    // let viewWidthFractioned 
+    // const [viweWidth, setViewWidth] = useState(windowWidth.size)
+    // const [viweWidth, setViewWidth] = useState()
+    // let viweWidth
+const [test, setTest] =useState(0)
+    if (typeof window !== "undefined") {
+        windowWidth.size = window.innerWidth
+        window.addEventListener('resize', () => {
+        windowWidth.size = window.innerWidth
+        setTest(windowWidth.size)
+        console.log(windowWidth.size)
+        })
+    }
+
     const loop = (array, height, delay) => {
         for (let i = 0; i < 10; i++) {
             array.push(
@@ -40,60 +49,73 @@ function NavBar() {
                     { animation: `blockByBlock 750ms ease-in-out ${i * delay}ms forwards` }
                 } key={i} className="blocks">
                     <img loading="eager" style={
-                        { objectPosition: `${-i * 50}px ${height}px` }
+                        { objectPosition: `${-i * test/10 }px ${height}px` }
                     } src={background} height="853" width="1280" alt=" a chilly plant" />
                 </div>)
             )
         }
+        
     }
-   
-
 
     useEffect(()=>{
+        if (typeof window !== "undefined") {
+            windowWidth.size = window.innerWidth
+            setTest(windowWidth.size)
+        }
 
-    },[])
+    },[windowWidth.size])
+
 
     return (
         <div>
+            <style jsx>{`
+              .backgroundImage {
+                width: ${test}px;
+              }
+              .backgroundImage .blocks {
+                    width: ${test/10}px;
+                    outline: 1px solid black;
+                        // height: 50px;
+                }
+                .backgroundImage .blocks img {
+                    width: ${test}px;
+                }
+            
+            
+            `}</style>
             <div className="backgroundImage">
                 {/* <img loading="eager" src={background} height="853" width="1280" alt=" a chilly plant" /> */}
-                {/* {populateArrays()} */}
+
                 {loop(arrayOfBlocks[1], 0, 50)}
+                {loop(arrayOfBlocks[2], -50, 100)}
+                {loop(arrayOfBlocks[3], -100, 150)}
+                {loop(arrayOfBlocks[4], -150, 200)}
+                {loop(arrayOfBlocks[5], -200, 250)}
+                {loop(arrayOfBlocks[6], -250, 300)}
+                {loop(arrayOfBlocks[7], -300, 350)}
+                {loop(arrayOfBlocks[8], -350, 400)}
+
                 {arrayOfBlocks[1].map((a) => (
                     a
                 ))}
-
-                {loop(arrayOfBlocks[2], -50, 100)}
                 {arrayOfBlocks[2].map((a) => (
                     a
                 ))}
-
-                {loop(arrayOfBlocks[3], -100, 150)}
                 {arrayOfBlocks[3].map((a) => (
                     a
                 ))}
-
-                {loop(arrayOfBlocks[4], -150, 200)}
                 {arrayOfBlocks[4].map((a) => (
                     a
                 ))}
-
-                {loop(arrayOfBlocks[5], -200, 250)}
                 {arrayOfBlocks[5].map((a) => (
                     a
                 ))}
-
-                {loop(arrayOfBlocks[6], -250, 300)}
                 {arrayOfBlocks[6].map((a) => (
                     a
                 ))}
-
-                {loop(arrayOfBlocks[7], -300, 350)}
                 {arrayOfBlocks[7].map((a) => (
                     a
                 ))}
-
-                {loop(arrayOfBlocks[8], -350, 400)}
                 {arrayOfBlocks[8].map((a) => (
                     a
                 ))}
@@ -115,6 +137,7 @@ function NavBar() {
                 {/* <div className="logo"> <img src={logo} height="100"  alt="A flame, the logo of Piiquante" /></div> */}
                 <Link to="/" className="title">
                     <h1>HOT TAKES</h1>
+                    <p>{test}</p>
                     {/* <p>THE WEB'S BEST HOT SAUCE REVIEWS</p> */}
                 </Link>
                 {/* <div className="logo"> <img src={logo} height="100" alt="A flame, the logo of Piiquante" /></div> */}
